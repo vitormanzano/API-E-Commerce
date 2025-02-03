af036a011945b2a7c14892d07a71c3c092d9f84e
Vitor Manzano Villela Dias <vimanzanovillela@gmail.com>
2025-02-02 21:56:08 -0300

    Adding validator if email already exists

--- src/repositories/prisma/prisma-person-repository.ts
+++ src/repositories/prisma/prisma-person-repository.ts
@@ -1,12 +1,19 @@
-import { Person, Prisma } from "@prisma/client";
+import { Prisma } from "@prisma/client";
 import { IPersonsRepository } from "../persons-repository-interface";
 import { prisma } from "../../lib/prisma";
 
 export class PrismaPersonRepository implements IPersonsRepository {
-    findPersonByEmail(email: string): Promise<Person> {
-        throw new Error("Method not implemented.");
+    async findPersonByEmail(email: string) {
+        const person = await prisma.person.findUnique({
+            where: {
+                email
+            }
+        });
+
+        return person
+
     }
-    async registerPerson(personData: Prisma.PersonCreateInput){
+    async registerPerson(personData: Prisma.PersonCreateInput) {
         const person = await prisma.person.create({
             data: personData
         });
