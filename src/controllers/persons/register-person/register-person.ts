82a7ed2f047e120bddc2f365324190b530711c14
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2025-02-04 19:28:11 -0300

    add validator for password lenght and for product quantity

--- src/controllers/persons/register-person/register-person.ts
+++ src/controllers/persons/register-person/register-person.ts
@@ -7,7 +7,7 @@ export const registerPerson = async (request: FastifyRequest, reply: FastifyRepl
     const registerBodySchema = z.object({
         name: z.string().min(1, "Name is mandatory"),
         email: z.string().email("Digit a valid email!"),
-        password: z.string().min(6, "Password should be have last 6 characters"),
+        password: z.string(),
         latitude: z.number().min(-90).max(90),
         longitude: z.number().min(-180).max(180)
     });
@@ -17,12 +17,20 @@ export const registerPerson = async (request: FastifyRequest, reply: FastifyRepl
     try {
         const registerPersonService = makeRegisterService();
 
-        await registerPersonService.execute(registerPersonBody);
+        const { person }  = await registerPersonService.execute(registerPersonBody);
+
+        return reply.status(201).send({
+            person: {
+                ...person,
+                password: undefined
+            }
+        });
     }
 
     catch (error) {
-       console.log(error); 
+       console.log(error);
+       return reply.status(400).send({ message: 'Bad request '}); 
     }
 
-    return reply.status(201).send({ message: "Usuário criado!" });
+    
 } 
--- src/services/person/register/register-person-service.ts
+++ src/services/person/register/register-person-service.ts
@@ -25,6 +25,10 @@ export class RegisterPersonService {
         if (hasExistPerson) {
             throw new PersonAlreadyExistsError();
         }
+        
+        if ( personData.password.length < 6) {
+            throw new Error('Password not be able to be fewer than 6 characters');
+        }
 
         const hashedPassword = await hash(personData.password, 6);
         personData.password = hashedPassword;
