ea6033bc7bde5cde9e230208a1bf6c9da0e7799e
Vitor Manzano Villela Dias <vimanzanovillela@gmail.com>
2025-02-05 19:35:53 -0300

    TDD for delete-person

---
+++ src/services/person/delete/delete-person-service.spec.ts
@@ -0,0 +1,32 @@
+import { describe, beforeEach, it, expect } from "vitest";
+import { IPersonsRepository } from "../../../repositories/persons-repository-interface";
+import { InMemoryPersonRepository } from "../../../repositories/in-memory/in-memory-person-repository";
+import { randomUUID } from "crypto";
+
+let personsRepository: IPersonsRepository;
+let sut: DeletePersonByGuidService;
+
+describe('Delete person service', () => {
+    beforeEach(async () => {
+        personsRepository = new InMemoryPersonRepository();
+        sut = new DeletePersonByGuidService(personsRepository);
+    });
+
+    it('Should be able to delete a person', async () => {
+        const personData = {
+            guid: randomUUID(),
+            name: 'John doe',
+            email: 'johndoe@gmail.com',
+            password: '123456',
+            latitude: -22.9482175,
+            longitude: -47.0652211 
+        }
+
+        await personsRepository.registerPerson(personData);
+
+        const deletedPerson = await sut.execute(delectedPerson);
+
+        expect(deletedPerson.guid).toEqual(expect.any(String));
+
+    });
+})
