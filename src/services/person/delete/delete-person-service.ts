73b9cedcabbcfa74ce40bdec30ccf35738827484
Vitor Manzano Villela Dias <vimanzanovillela@gmail.com>
2025-02-05 21:27:56 -0300

    Creating interfaces for request and response in authenticate

--- src/services/person/delete/delete-person-service.ts
+++ src/services/person/delete/delete-person-service.ts
@@ -2,16 +2,26 @@ import { Person } from "@prisma/client";
 import { IPersonsRepository } from "../../../repositories/persons-repository-interface";
 import { InvalidCredentialsError } from "../../../errors/invalid-credentials-error";
 
+interface IDeletePersonByGuidRequest {
+    guid: string
+}
+
+interface IDeletePersonByGuidResponse {
+    person: Person;
+}
+
 export class DeletePersonByGuidService {
     constructor(private personsRepository: IPersonsRepository) {}
 
-    async execute(guid: string): Promise<Person> {
-        const hasExistPerson = await this.personsRepository.deletePersonByGuid(guid);
+    async execute({guid}: IDeletePersonByGuidRequest): Promise<IDeletePersonByGuidResponse> {
+        const person = await this.personsRepository.deletePersonByGuid(guid);
 
-        if (!hasExistPerson) {
+        if (!person) {
             throw new InvalidCredentialsError(); 
         }
 
-        return hasExistPerson
+        return {
+            person
+        }
     }
 }
