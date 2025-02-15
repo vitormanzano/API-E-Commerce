d11a1659c9d4ac76a7d944017fd563eb26e0e09f
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2025-02-15 10:53:45 -0300

    Update person route working

--- src/services/person/update/update-person-service.ts
+++ src/services/person/update/update-person-service.ts
@@ -1,6 +1,6 @@
 import { IPersonsRepository } from "@/repositories/persons-repository-interface";
 
-interface IUpdatePersonRequest {
+interface IUpdatePersonBodyRequest {
     email?: string,
     name?: string,
     password?: string,
@@ -8,11 +8,10 @@ interface IUpdatePersonRequest {
     longitude?: number
 }
 
-
-export class  UpdatePersonService {
+export class UpdatePersonService {
     constructor(private personsRepository: IPersonsRepository) {}
 
-    async execute(guid: string, person: IUpdatePersonRequest) {
+    async execute(guid: string, person: IUpdatePersonBodyRequest) {
         const updatedPerson = await this.personsRepository.updatePersonByGuid(guid, person)
 
         return { updatedPerson }
