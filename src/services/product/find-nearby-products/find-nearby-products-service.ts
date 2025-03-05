14cb969443e7449bcca4e9810622c9511ef1ee30
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2025-03-04 21:53:56 -0300

    nearby products workings but without test

---
+++ src/services/product/find-nearby-products/find-nearby-products-service.ts
@@ -0,0 +1,31 @@
+import { IPersonsRepository } from "@/repositories/persons-repository-interface";
+import { IProductsRepository } from "@/repositories/products-repository-interface";
+import { IFindNearbyProductsServiceResponse } from "./models/IFindNearbyProductsServiceResponse";
+
+export class FindNearbyProductsService {
+    constructor(private personsRepository: IPersonsRepository,
+                private productsRepository: IProductsRepository
+    ) {}
+
+    async execute(personGuid: string): Promise<IFindNearbyProductsServiceResponse> {
+        const person = await this.personsRepository.findPersonByGuid(personGuid);
+
+        if (!person) {
+            throw new Error();
+        }
+
+        const nearbySellers = await this.personsRepository.findNearbySellers({
+            personGuid,
+            latitude: Number(person.latitude),
+            longitude: Number(person.longitude)
+        });
+
+        const nearbyProducts = (await Promise.all(nearbySellers.flatMap(person => 
+            this.productsRepository.findProductsByPerson(person.guid, 1)
+        ))).flat();
+
+        return { nearbyProducts } 
+    }
+
+
+}
