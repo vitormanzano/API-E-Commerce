156d68781c921d819ddee5ac9d17ab4d76024724
Vitor Manzano <vimanzanovillela@gmail.com>
2025-03-10 16:57:23 -0300

    refactoring profile controller

--- src/controllers/persons/profile/profile.ts
+++ src/controllers/persons/profile/profile.ts
@@ -1,19 +1,26 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 import { makeGetPersonProfileService } from "@/factories/make-get-person-profile-service";
+import * as HttpResponse from "@/utils/http-helper";
 
 export const profile = async (request: FastifyRequest, reply: FastifyReply) => {
-   const getPersonProfile = makeGetPersonProfileService();
+   try {
+        const getPersonProfile = makeGetPersonProfileService();
 
-   const guid = request.user.sub;
+        const guid = request.user.sub;
 
-    const { person } = await getPersonProfile.execute({ guid });
+        const { person } = await getPersonProfile.execute({ guid });
 
-    return reply.status(200).send({
-        person: {
+        const httpResponse = await HttpResponse.ok({
             ...person,
             guid: undefined,
             password: undefined
-        }
-    });
+        });
+
+    return reply.status(httpResponse.statusCode).send(httpResponse.body);
+   } 
+   catch (error) {
+        const httpResponse = await HttpResponse.badRequest(error);
+        return reply.status(httpResponse.statusCode).send(httpResponse.body);
+   }
 
 }   
