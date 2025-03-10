a2a194f82059e2533f74022bb2f11f3239798111
Vitor Manzano <vimanzanovillela@gmail.com>
2025-03-10 17:24:57 -0300

    Find all products request model

--- src/services/product/find-all-products/find-all-products-service.ts
+++ src/services/product/find-all-products/find-all-products-service.ts
@@ -1,11 +1,12 @@
 import { ResourceNotFoundError } from "@/errors/resource-not-found-error";
 import { IProductsRepository } from "@/repositories/products-repository-interface";
 import { IFindAllProductsServiceResponse } from "./models/IFindAllProductsServiceRespose";
+import { IFindAllProductsServiceRequest } from "./models/IFindAllProductsServiceRequest";
 
 export class FindAllProductsService {
     constructor(private productsRepository: IProductsRepository) {}
 
-    async execute(page: number): Promise<IFindAllProductsServiceResponse> {
+    async execute({page}: IFindAllProductsServiceRequest): Promise<IFindAllProductsServiceResponse> {
         const allProducts = await this.productsRepository.findAllProducts(page);
 
         if (allProducts.length === 0) {
---
+++ src/services/product/find-all-products/models/IFindAllProductsServiceRequest.d.ts
@@ -0,0 +1,3 @@
+export interface IFindAllProductsServiceRequest {
+    page: number;
+}
