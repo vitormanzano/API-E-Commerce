32d1f2a8245ed40b1dcd0e62874ee287e4aae2a3
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2025-03-18 16:53:34 -0300

    find all products and find nearby products tests

--- src/services/product/find-all-products/find-all-products-service.spec.ts
+++ src/services/product/find-all-products/find-all-products-service.spec.ts
@@ -58,10 +58,55 @@ describe('Find all products', () => {
         expect(allProducts).toHaveLength(2);
     });
 
+    it('Should be able to paginate all products', async () => {
+        const personData = {
+            guid: randomUUID(),
+            name: 'John doe',
+            email: 'johndoe@gmail.com',
+            password: '123456',
+            latitude: -22.9482175,
+            longitude: -47.0652211 
+        }
+
+        await personsRepository.registerPerson(personData);
+
+
+        const sellerData = {
+                    guid: randomUUID(),
+                    name: 'John doe',
+                    email: 'johndoe@gmail.com',
+                    password: '123456',
+                    latitude: -22.9482175,
+                    longitude: -47.0652211 
+        }
+        
+        const seller = await personsRepository.registerPerson(sellerData);
+        
+        for (let i = 1; i <= 22; i++) {
+            await productsRepository.registerProduct({
+                name: `Vitor ${i}`,
+                description: `Something ${i}`,
+                price: 10.00,
+                quantity: 1,
+                sellerId: seller.guid
+            });
+        }
+
+        const page = 2;
+
+        const { allProducts } = await sut.execute({page})
+
+        expect(allProducts).toHaveLength(2);
+        expect(allProducts).toEqual([
+            expect.objectContaining({ name: "Vitor 21" }),
+            expect.objectContaining({ name: "Vitor 22" })
+        ])
+    })
+
+
     it('Should not be able to find products if not exist anyone product', async () => {
         const page = 1;
         await expect( () => sut.execute({page})).rejects.toBeInstanceOf(ResourceNotFoundError);
     });
 
-    
 })
